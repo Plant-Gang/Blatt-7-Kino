12f9e667a182bb14c3586f8507e71cc89d348688
Plant-Gang <danielolleschedv@web.de>
2026-06-23 15:21:58 +0200

    Added comments

--- src/observers/Observable.java
+++ src/observers/Observable.java
@@ -8,11 +8,18 @@ public abstract class Observable
 
     private List<Observer> observers = new ArrayList<>();
 
+    /**
+     * Adds the given Observer to the list of Observers
+     * @param observer
+     */
     public void addObserver(Observer observer)
     {
         observers.add(observer);
     }
 
+    /**
+     * Notifies all subscribed Observers when an event occurs
+     */
     protected void update(Observable this)
     {
         for (Observer o : observers)
--- src/observers/Observer.java
+++ src/observers/Observer.java
@@ -2,5 +2,10 @@ package observers;
 
 public interface Observer
 {
+    /**
+     * Tells the Observable to add this Observer to the list of subscribers
+     * 
+     * @param observable the observable this Observer wants to subscribe to
+     */
     public void listen(Observable observable);
 }
